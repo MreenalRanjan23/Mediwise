d78803b838aa46650a40d6fc6f01a12dfbdbfad0
MreenalRanjan23 <mreenalranjan@gmail.com>
2026-06-07 17:44:12 +0530

    Create application landing page

---
+++ frontend/app/page.tsx
@@ -0,0 +1,9 @@
+import MainLayout from "@/components/layout/MainLayout";
+
+export default function Home() {
+  return (
+    <MainLayout>
+      <h1 className="text-3xl font-bold">Dashboard</h1>
+    </MainLayout>
+  );
+}
